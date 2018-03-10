cfad61731c15ac52a1a7db11ce5c0f25d5fafc2b
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-10 14:52:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,28 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, element){
+  
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  
+}
+function addElementToEndOfArray(array, element){
+  
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
+function accessElementInArray(array, index){
+  
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  
+}
+function removeElementFromBeginningOfArray(array){
+  
+}
+function destructivelyAddElementToEndOfArray(array){
+  
+}
+function removeElementFromEndOfArray(array){
+  
+}
