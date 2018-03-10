50d3141d0b5c9bb893cdc467f1e5dd207035689f
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-10 15:02:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
-
-}
+    var myarray = array;
+    return [element, ...myarray];
+  }
 function destructivelyAddElementToBeginningOfArray(array, element){
-
+      
 }
 function addElementToEndOfArray(array, element){
 
