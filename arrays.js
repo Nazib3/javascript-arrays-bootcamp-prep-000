13a73e6833363584d570f07d21692dd904043049
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-10 15:24:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,10 +20,11 @@ function accessElementInArray(array, index){
     return array[index];
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-
+    array.shift();
+    return array;
 }
 function removeElementFromBeginningOfArray(array){
-
+    return array.slice(1);
 }
 function destructivelyAddElementToEndOfArray(array){
 
